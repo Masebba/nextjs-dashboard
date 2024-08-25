9efff7c0ddcf2558a8b3ecd2263f48e634584a1b
Masebba <masternasser2@gmail.com>
2024-08-25 12:15:26 +0300

    adding new .env

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -7,7 +7,7 @@ const client = await db.connect();
 async function seedUsers() {
   await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
   await client.sql`
-CREATE TABLE IF NOT EXISTS users (
+     CREATE TABLE IF NOT EXISTS users (
        id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
        name VARCHAR(255) NOT NULL,
        email TEXT NOT NULL UNIQUE,
@@ -102,10 +102,7 @@ async function seedRevenue() {
 }
 
 export async function GET() {
-  return Response.json({
-    message:
-      'Uncomment this file and remove this line. You can delete this file when you are finished.',
-  });
+
   try {
     await client.sql`BEGIN`;
     await seedUsers();
@@ -113,7 +110,7 @@ export async function GET() {
     await seedInvoices();
     await seedRevenue();
     await client.sql`COMMIT`;
-0 
+
     return Response.json({ message: 'Database seeded successfully' });
   } catch (error) {
     await client.sql`ROLLBACK`;
